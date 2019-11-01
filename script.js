5a3a73701b4ffe7aecdcbb6b47f88355c310b9e9
Ania Jelińska <ania.jelin@wp.pl>
2019-11-01 17:37:47 +0100

    Handled exception when response is false. Generated html tags with results

--- script.js
+++ script.js
@@ -1,50 +1,93 @@
-async function search(title, year, sortByProperty) {
-  try{
-    let url = `https://www.omdbapi.com/?s=${title}&y=${year}&apikey=6a0ebf5d`;
-    let response = await fetch(url);
-    const data = await response.json();
-    const z = sortBy(data, sortByProperty);
+ document.getElementById('btn-search').addEventListener('click', search);
 
-    console.log(z);
-  } catch (error) {
-    console.log(error);
+const state = {
+    lastNumberOfResults: 0,
+    data: []
   }
-}
-
-function sortBy(data, sortByProperty){
-  if (sortByProperty === "Title") {
-    return sortByTitle(data);
+  
+  
+  async function search(title, year, sortByProperty) {
+    try{
+      let url = `https://www.omdbapi.com/?s=${title}&y=${year}&apikey=6a0ebf5d`;
+      let response = await fetch(url);
+      const data = await response.json();
+      if(data.Response === "False") {
+        return console.log(data.Error)
+      } 
+      
+      state.data = sortBy(data, sortByProperty);
+  
+    } catch (error) {
+      console.log(error);
+    }
   }
-  if(sortByProperty === "Year"){
-    return sortByYear(data);
+  
+  function sortBy(data, sortByProperty){
+    if (sortByProperty === "Title") {
+      return sortByTitle(data);
+    }
+    if(sortByProperty === "Year"){
+      return sortByYear(data);
+    }
+    return data;
   }
-  return data;
-}
+  
+  function sortByTitle(data){
+     return data.Search.sort((a,b) => {
+        if(a.Title < b.Title) 
+          return -1; 
+        if(a.Title > b.Title) 
+          return 1;
+  
+        return 0;
+      })
+  }
+  
+  function sortByYear(data){
+     return data.Search.sort((a,b) => {
+        if(a.Year < b.Year) 
+          return -1; 
+        if(a.Year > b.Year) 
+          return 1;
+  
+        return 0;
+      })
+  }
+  
+  
+  search("day", "", "Year")
+  
+  
+  function showSlicedResult (){
+    let newNumberOfResult = state.lastNumberOfResults + 1;
+    state.lastNumberOfResults = newNumberOfResult;
+  
+    let slicedResult = state.data.slice(0,newNumberOfResult);
+    let htmlTags = slicedResult
+      .map(elem => `<tr>
+        <td>
+            <img src=${elem.Poster}/>
+        </td>
+        <td>${elem.Title}</td>
+        <td>${elem.Year}</td>
+        </tr>`)
+      .reduce( (p,n) => p + n);
+  
+    let output = `<table>
+    <tr>
+      <th>Title</th>
+      <th>Year</th>
+      <th>Poster</th>
+    </tr>
+  ${htmlTags}
+  </table>`
+  
+    console.log(output);
+  }
 
-function sortByTitle(data){
-   return data.Search.sort((a,b) => {
-      if(a.Title < b.Title) 
-        return -1; 
-      if(a.Title > b.Title) 
-        return 1;
-
-      return 0;
-    })
-}
-
-function sortByYear(data){
-   return data.Search.sort((a,b) => {
-      if(a.Year < b.Year) 
-        return -1; 
-      if(a.Year > b.Year) 
-        return 1;
-
-      return 0;
-    })
-}
 
 
-doSomethign("Avengers", '', "");
+
 
 
 
